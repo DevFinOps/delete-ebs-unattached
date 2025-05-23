1e331de709c7c3c9412cdc9418ef97fea2debe75
Vitor Esteves <vesteves33@gmail.com>
2025-05-23 09:08:13 -0300

    feat: adicionando o output do sns no modulo tf

--- iac/main.tf
+++ iac/main.tf
@@ -18,6 +18,15 @@ output "nome_da_funcao_lambda" {
   description = "Nome da função Lambda implantada (referenciado do módulo)"
 }
 
+output "arn_do_topico_sns" {
+  value       = module.lambda_s3_deploy.sns_topic_arn
+  description = "ARN do tópico SNS (referenciado do módulo)"
+}
+output "nome_do_topico_sns" {
+  value       = module.lambda_s3_deploy.sns_topic_name
+  description = "Nome do tópico SNS (referenciado do módulo)"
+}
+
 
 
 
